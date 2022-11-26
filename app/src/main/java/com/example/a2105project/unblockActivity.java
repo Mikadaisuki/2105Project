b59abd418576a4178b4cfa6d157487d2beec6440
Mika <30582811+Mikadaisuki@users.noreply.github.com>
2022-11-26 18:48:51 -0500

    update

--- app/src/main/java/com/example/a2105project/unblockActivity.java
+++ app/src/main/java/com/example/a2105project/unblockActivity.java
@@ -21,19 +21,23 @@ import java.util.Map;
 
 public class unblockActivity extends AppCompatActivity {
 
-    private ListView unblockView;
+    private ListView blockList;
 
     private List<Account> accounts = new LinkedList<>();
+
     private DatabaseReference susAccountRef;
     private FirebaseDatabase firebaseDatabase ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        unblockView = (ListView)findViewById(R.id.unblockView);
-
         setContentView(R.layout.activity_unblock);
+
         susAccountRef = firebaseDatabase.getInstance().getReference("Account");
+
+        blockList = (ListView) findViewById(R.id.blockList);
+
+        System.out.println(blockList);
 
         susAccountRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -44,15 +48,19 @@ public class unblockActivity extends AppCompatActivity {
                     Account account = child.getValue(Account.class);
                     if(account.getStatus().equals("tempFalse")||account.getStatus().equals("False")){
                         accounts.add(account);
+
+                        Map<String, String> dataMap = new HashMap<>();
+                        dataMap.put("Email",account.getEmail());
+                        data.add(dataMap);
                     }
-                    Map<String, String> dataMap = new HashMap<>();
-                    dataMap.put("id",account.getEmail());
-                    data.add(dataMap);
                 }
-                SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(),data,R.layout.unblock_list,
-                        new String[]{"id"}, new int []{R.id.SusAccounts});
                 System.out.println(accounts);
-                unblockView.setAdapter(adapter);
+                System.out.println(data);
+                SimpleAdapter sus = new SimpleAdapter(getApplicationContext(),data,R.layout.unblock_list,
+                        new String[]{"Email"}, new int []{R.id.susEmail});
+                System.out.println(sus);
+
+                blockList.setAdapter(sus);
             }
 
             @Override
@@ -61,5 +69,4 @@ public class unblockActivity extends AppCompatActivity {
             }
         });
     }
-
 }
