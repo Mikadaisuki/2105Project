7148d8ff000be0afcebccae9cfa6d82876df1aa5
Rongyuan Tian <113475661+IRIS879@users.noreply.github.com>
2022-11-26 15:23:57 -0500

    LOL1

--- app/src/main/java/com/example/a2105project/adminPageActivity.java
+++ app/src/main/java/com/example/a2105project/adminPageActivity.java
@@ -1,14 +1,15 @@
 package com.example.a2105project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class adminPageActivity extends AppCompatActivity {
     Button ComBtn;
+    Button SusBtn;
     // jump to the administration interface
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +17,21 @@ public class adminPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_page);
 
         ComBtn = (Button) findViewById(R.id.comBtn);
+        SusBtn = (Button) findViewById(R.id.susBtn);
 
         ComBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
                 intent.setClass(adminPageActivity.this, complaintsView_Activity.class);
+                startActivity(intent);
+            }
+        });
+        SusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(adminPageActivity.this, unblockActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/a2105project/unblockActivity.java
+++ app/src/main/java/com/example/a2105project/unblockActivity.java
@@ -1,14 +1,65 @@
 package com.example.a2105project;
 
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.a2105project.Entity.Account;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class unblockActivity extends AppCompatActivity {
+
+    private ListView unblockView;
+
+    private List<Account> accounts = new LinkedList<>();
+    private DatabaseReference susAccountRef;
+    private FirebaseDatabase firebaseDatabase ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        unblockView = (ListView)findViewById(R.id.unblockView);
+
         setContentView(R.layout.activity_unblock);
+        susAccountRef = firebaseDatabase.getInstance().getReference("Account");
+
+        susAccountRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                accounts.clear();
+                List<Map<String, String >> data = new LinkedList<>();
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    Account account = child.getValue(Account.class);
+                    if(account.getStatus().equals("tempFalse")||account.getStatus().equals("False")){
+                        accounts.add(account);
+                    }
+                    Map<String, String> dataMap = new HashMap<>();
+                    dataMap.put("id",account.getEmail());
+                    data.add(dataMap);
+                }
+                SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(),data,R.layout.unblock_list,
+                        new String[]{"id"}, new int []{R.id.SusAccounts});
+                System.out.println(accounts);
+                unblockView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
 }
